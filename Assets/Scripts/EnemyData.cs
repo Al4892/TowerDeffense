feee26b18f5e3ea8ffe8e4963afa8f37fcaa211d
Al4892 <alan.iniguez@uteca.edu.mx>
2025-07-18 18:27:34 -0600

    xd

---
+++ Assets/Scripts/EnemyData.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyData", menuName = "Scriptable Objects/EnemyData")]
+public class EnemyData : ScriptableObject
+{
+    public float attackRange = 1f;
+    public float AttackDamage = 10f;
+    public float runSpeed = 2f;
+    public float attackDuration = 1f;
+    public float attackCooldown = 1f;
+    public string PrimaryTargetTag = "Tower";
+    public string runAnimationName = "zombieRun";
+    public string attackAnimationName = "ZombieAttack";
+    public string dieAnimationName = "ZombieDie";
+}
